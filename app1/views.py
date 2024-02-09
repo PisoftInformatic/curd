7a3d09960b6f7e39b967819064b5cdc4b1e8dac9
PisoftInformatic <anjupisoft@gmail.com>
2024-02-09 11:20:47 +0530

    message : updated update_server

--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import git
 
+
+def update_server(request):
+    if request.method == 'POST':
+        try:
+            repo = git.Repo('https://github.com/PisoftInformatic/curd.git')
+            origin = repo.remotes.origin
+            origin.pull()
+            return HttpResponse('Updated Django server successfully', status=200)
+        except Exception as e:
+            return HttpResponse(str(e), status=500)
+    else:
+        return HttpResponseBadRequest('Wrong event type')
 
 def index(requests):
     return HttpResponse("Hello This is Home Page")
 
-
-def update_server(requests):
-    return HttpResponse("Server Updated")
